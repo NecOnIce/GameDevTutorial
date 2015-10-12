15e1b020f11cb50bb81e14ee9ce2078761795b63
Jonas Scherbaum <3blalerpo@googlemail.com>
2015-10-12 11:15:49 +0200

    Added first GameApplication Main Class.

---
+++ src/main/java/com/jsappartment/game/GameApplication.java
@@ -0,0 +1,13 @@
+package com.jsappartment.game;
+
+/**
+ * Created by Jonas on 12.10.2015.
+ *
+ * This is the main Application Class for the whole Game.
+ *
+ * @author Jonas Scherbaum
+ */
+public class GameApplication {
+
+
+}
